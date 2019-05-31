2d1ab4048e11c8244aabf5919c6d15d001374085
Kani Omurzakova <kkadyrberdieva@gmail.com>
2019-05-31 00:30:25 +0000

    new changes

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
-var pluralize = require('pluralize')
+
 var words = ['dog', 'person', 'moose', 'ox', 'wife', 'guest', 'man', 'woman', 'mess', 'photo', 'avocado', 'superhero', 'bar', 'monkey', 'combo', 'toy', 'chair', 'toe'];
 
 function getPluralization(word) {
-    // This needs to be replaced with the code that returns a pluralized version of the input word
-    if (word=="person") {
-        return "people";
-    } else if (word=="moose") {
-        return "moose";
-    } else if (word=="ox") {
-        return "oxen";
-    } else if (word=="wife") {
-        return "wives";
-    } else if (word=="man") {
-        return "men";
-    } else if (word=="woman") {
-        return "women";
-    } else if (word=="mess") {
-        return "messes";
-    } else if (word=="woman") {
-        return "women";
-    } else {
-        return word + "s"
-        
-    }
+   var pluralize = require('pluralize');
+   return pluralize (word);
 }
 
 function printPluralizedWord(word) {
